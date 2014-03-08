6c2e404b13546a879e6ccfdf5ad5673106ce1452
翟会昌 <zhaihuichang@hotmail.com>
2014-03-08 13:06:48 +0800

    Admin Home

---
+++ EnterpriseSystemASPXBg/Controllers/AdminAccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnterpriseSystemASPX;
+using EnterpriseSystemASPX.BLL;
+using EnterpriseSystemASPX.Models;
+
+namespace EnterpriseSystemASPXBg.Controllers
+{
+    public class AdminAccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login(string adminname, string password)
+        {
+            if (string.IsNullOrEmpty(adminname) && string.IsNullOrEmpty(password))
+            {
+                ViewBag.Message = "请登录";
+            }
+            else 
+            {
+                MEnterpriseAdmin admin = BLLMEnterpriseAdmin.AdminLogin(adminname, password);
+                if (admin == null)
+                {
+                    ViewBag.Message = "登录失败";
+                }
+                else
+                {
+                    ViewBag.Message = "已经登录";
+                }
+            }
+            return View();
+        }
+
+    }
+}
