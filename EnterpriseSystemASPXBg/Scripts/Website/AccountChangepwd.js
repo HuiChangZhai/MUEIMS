e65f54e61682cfec0dbd5603f5eb8b22f607dfc2
JackSnowolf <zhaihuichang@hotmail.com>
2014-03-11 22:00:12 +0800

    changepwd.js

---
+++ EnterpriseSystemASPXBg/Scripts/Website/AccountChangepwd.js
@@ -0,0 +1,16 @@
+﻿function SaveChangepwd() {
+    var newpwd = $("#newpwd").val();
+    var oldpwd = $("#comfirmpwd").val();
+    if (newpwd.trim() == "")
+        $("#NewpwdError").html("请输入新密码");
+    else
+        $("#NewpwdError").html("");
+    if (oldpwd.trim() == "")
+        $("#ComfirmError").html("请输入确认密码");
+    else if (oldpwd.trim() != newpwd.trim())
+        $("#ComfirmError").html("密码不一致");
+    else
+        $("#ComfirmError").html("");
+    if ($("#NewpwdError").html() == "" && $("#ComfirmError").html() == "")
+        $("#formChangepwd").submit();
+}
