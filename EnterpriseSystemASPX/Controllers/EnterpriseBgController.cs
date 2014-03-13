5a790ae874ee947a478c6c295ec5bc430116a2e8
翟会昌 <zhaihuichang@hotmail.com>
2014-03-13 23:34:26 +0800

    ueditor

--- EnterpriseSystemASPX/Controllers/EnterpriseBgController.cs
+++ EnterpriseSystemASPX/Controllers/EnterpriseBgController.cs
@@ -18,68 +18,75 @@ namespace EnterpriseSystemASPX.Controllers
         {
             List<Enterprise> enterpriseList = BLLEnterprise.GetEnterpriseList(0, PageSize);
             ViewBag.EnterpriseList = enterpriseList;
-            return View("~/Views/EnterpriseBg/EnterpriseInfos.aspx");
+            return View("~/Views/EnterpriseBg/EnterpriseInfo.aspx");
         }
-
-        public ActionResult EnterpriseInfos(int? pageCount)
-        {
-            pageCount = pageCount ?? 0;
-            List<Enterprise> enterpriseList = BLLEnterprise.GetEnterpriseList(pageCount.Value, PageSize);
-            ViewBag.EnterpriseList = enterpriseList;
-            ViewBag.CurrentPage = pageCount.Value;
-            return View();
-        }
-
-        public ActionResult EnterpriseInfoDetail(int? EnterpriseID)
-        {
-            return View();
-        }
-
-        public ActionResult EnterpriseInfoEdit(int? EnterpriseID)
-        {
-            EnterpriseID = EnterpriseID ?? 0;
-            ViewBag.EnterpriseID = EnterpriseID.Value;
-            
-            return View();
-        }
-
+        
         public ActionResult EnterpriseInfo()
         {
+            ViewBag.MenuGroup = "Info";
+            ViewBag.PageTitle = "企业信息";
+
             return View();
         }
 
         public ActionResult EnterpriseInfoEdit()
         {
+            ViewBag.EnterpriseID = BLLEnterprise.Current.EnterpriseID;
+            ViewBag.MenuGroup = "Info";
+            ViewBag.PageTitle = "编辑企业信息";
+
             return View();
         }
 
+        public ActionResult EnterpriseBrief()
+        {
+            ViewBag.MenuGroup = "Info";
+            ViewBag.PageTitle = "企业简介";
+
+            return View();
+        }
+
+        
+
         public ActionResult EnterpriseCaseList()
         {
+            ViewBag.MenuGroup = "Case";
+            ViewBag.PageTitle = "成功案例";
             return View();
         }
 
         public ActionResult EnterpriseCaseEdit()
         {
+            ViewBag.MenuGroup = "Case";
+            ViewBag.PageTitle = "编辑成功案例";
             return View();
         }
 
         public ActionResult EnterpriseCaseAdd()
         {
+            ViewBag.MenuGroup = "Case";
+            ViewBag.PageTitle = "添加成功案例";
             return View();
         }
 
         public ActionResult EnterpriseDynamicList()
         {
+            ViewBag.MenuGroup = "Dynamic";
+            ViewBag.PageTitle = "企业动态";
             return View();
         }
 
         public ActionResult EnterpriseDynamicEdit()
         {
+            ViewBag.MenuGroup = "Dynamic";
+            ViewBag.PageTitle = "编辑企业动态";
             return View();
         }
 
         public ActionResult EnterpriseDynamicAdd()
         {
+            ViewBag.MenuGroup = "Dynamic";
+            ViewBag.PageTitle = "添加企业动态";
             return View();
         }
     }
--- EnterpriseSystemASPX/Views/EnterpriseBg/EnterpriseInfoEdit.aspx
+++ EnterpriseSystemASPX/Views/EnterpriseBg/EnterpriseInfoEdit.aspx
@@ -4,11 +4,15 @@
     EnterpriseInfoEdit
 </asp:Content>
 
+<asp:Content ID="Content3" ContentPlaceHolderID="ScriptAndStyleContent" runat="server">
+    <script src="/Content/ueditor1_3_6-utf8-net/ueditor.config.js"></script>
+    <script src="/Content/ueditor1_3_6-utf8-net/ueditor.all.min.js"></script>
+</asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
 
-<h2>EnterpriseInfoEdit</h2>
+<h2><%:ViewBag.EnterpriseID %></h2>
 
 </asp:Content>
 
-<asp:Content ID="Content3" ContentPlaceHolderID="ScriptAndStyleContent" runat="server">
-</asp:Content>
+
