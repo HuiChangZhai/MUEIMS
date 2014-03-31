6d875f4cb33d3b7cf9d6852ccbd57eed8ca99d59
JackSnowolf <zhaihuichang@hotmail.com>
2014-03-31 23:38:48 +0800

    上传 上传logo没长传的部分

---
+++ EnterpriseSystemASPXBg/Controllers/FileUploaderBgController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnterpriseSystemASPXBg.Controllers
+{
+    public class FileUploaderBgController : Controller
+    {
+        [HttpPost]
+        public string Index(HttpPostedFileBase file, HttpPostedFileBase enterpriseLogo)
+        {
+            string now = "";//DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString() + (new Random().Next(1000,10000));
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the fielname
+                var fileName = now + Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/uploadImages"), fileName);
+                file.SaveAs(path);
+                return fileName;
+            }
+            else if (enterpriseLogo != null && enterpriseLogo.ContentLength > 0)
+            {
+                // extract only the fielname
+                var fileName = now + Path.GetFileName(enterpriseLogo.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/uploadImages"), fileName);
+                enterpriseLogo.SaveAs(path);
+                return fileName;
+            }
+
+            return "";
+        }
+    }
+}
